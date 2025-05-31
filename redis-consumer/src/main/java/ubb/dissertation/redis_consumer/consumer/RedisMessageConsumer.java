349a489379017ef2adeac4714b5f413d8938a095
Alexandra Bledea <bledea_alexandra@yahoo.com>
2025-05-31 22:25:38 +0300

    Final version of Redis

--- redis-consumer/src/main/java/ubb/dissertation/redis_consumer/consumer/RedisMessageConsumer.java
+++ redis-consumer/src/main/java/ubb/dissertation/redis_consumer/consumer/RedisMessageConsumer.java
@@ -1,43 +1,35 @@
 package ubb.dissertation.redis_consumer.consumer;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ubb.dissertation.common.Message;
+import ubb.dissertation.common.OshiLogger;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-@Slf4j
 @Component
 public class RedisMessageConsumer {
 
-    private int received = 0;
-    private long start = 0;
-    private final List<Long> latencies = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(RedisMessageConsumer.class);
+
+    private final OshiLogger oshiLogger;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+
+    public RedisMessageConsumer() throws IOException {
+        this.oshiLogger = new OshiLogger("results.csv");
+        scheduler.scheduleAtFixedRate(oshiLogger::log, 0, 1, TimeUnit.SECONDS);
+    }
 
     public void onMessage(Message message) {
-        System.out.println("Received message: " + message);
         long now = System.currentTimeMillis();
         long latency = now - message.getTimestamp();
-        latencies.add(latency);
+        oshiLogger.recordMessage(latency);
 
-        if (received == 0) {
-            start = now;
-        }
-
-        received++;
         log.info("Redis: Received message {} of {}, latency: {} ms", message.getMessageNumber(), message.getNumberOfMessages(), latency);
-
-        if (received == message.getNumberOfMessages()) {
-            long duration = now - start;
-            double throughput = (double) received / (duration / 1000.0);
-            double avgLatency = latencies.stream().mapToLong(Long::longValue).average().orElse(0);
-
-            log.info("Redis Batch Complete - Latency: avg ~{}ms, Throughput: {}msg/sec",
-                    avgLatency, throughput);
-
-            received = 0;
-            latencies.clear();
-        }
     }
 }
