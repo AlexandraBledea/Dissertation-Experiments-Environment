a35a277a4b7816e1f4410ea9aa15b2e43adeb2c3
Alexandra Bledea <bledea_alexandra@yahoo.com>
2025-05-31 22:18:01 +0300

    Added final version for rabbit mq

--- rabbitmq-consumer/src/main/java/ubb/dissertation/rabbitmq_consumer/consumer/RabbitMessageConsumer.java
+++ rabbitmq-consumer/src/main/java/ubb/dissertation/rabbitmq_consumer/consumer/RabbitMessageConsumer.java
@@ -1,44 +1,37 @@
 package ubb.dissertation.rabbitmq_consumer.consumer;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import ubb.dissertation.common.Message;
+import ubb.dissertation.common.OshiLogger;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-@Slf4j
 @Component
 public class RabbitMessageConsumer {
 
-    private int received = 0;
-    private long start = 0;
-    private final List<Long> latencies = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(RabbitMessageConsumer.class);
+
+    private final OshiLogger oshiLogger;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+
+    public RabbitMessageConsumer() throws IOException {
+        this.oshiLogger = new OshiLogger("results.csv");
+        scheduler.scheduleAtFixedRate(oshiLogger::log, 0, 1, TimeUnit.SECONDS);
+    }
 
     @RabbitListener(queues = "${rabbit.queue-name}")
     public void receiveMessage(Message message) {
         long now = System.currentTimeMillis();
         long latency = now - message.getTimestamp();
-        latencies.add(latency);
+        oshiLogger.recordMessage(latency);
 
-        if (received == 0) {
-            start = now;
-        }
-
-        received++;
         log.info("RabbitMQ: Received message {} of {}, latency: {} ms", message.getMessageNumber(), message.getNumberOfMessages(), latency);
-
-        if (received == message.getNumberOfMessages()) {
-            long duration = now - start;
-            double throughput = duration > 0 ? (double) received / (duration / 1000.0) : 0;
-            double avgLatency = latencies.stream().mapToLong(Long::longValue).average().orElse(0);
-
-            log.info("RabbitMQ Batch Complete - Latency: avg ~{}ms, Throughput: {}msg/sec",
-                    avgLatency, throughput);
-
-            received = 0;
-            latencies.clear();
-        }
     }
 }
